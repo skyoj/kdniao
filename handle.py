2b5a9275bfeef19b8902cb4d864693deaca81e8d
Xiao <voewjwz@sina.com>
2017-01-19 17:25:00 +0800

    fix

--- handle.py
+++ handle.py
@@ -9,18 +9,17 @@ import datetime
 
 class Handle(object):
     def POST(self):
-        try:
-            webData = web.data()
-            print type(webData)
-            print "Handle Post webdata is", webData
-            reqData = receive.parse_xml(webData)
-            print reqData
-            request_data = reqData.RequestData
-            data = json.loads(request_data)
-            res_data = {
-                'EBusinessID': data['EBusinessID'],
-                'Success': 'true',
-                'UpdateTime': datetime.datetime.now().strftime("%Y-%m-%d" "%H:%M:%S")
-            }
-            print res_data
-            return res_data
+        webData = web.data()
+        print type(webData)
+        print "Handle Post webdata is", webData
+        reqData = receive.parse_xml(webData)
+        print reqData
+        request_data = reqData.RequestData
+        data = json.loads(request_data)
+        res_data = {
+            'EBusinessID': data['EBusinessID'],
+            'Success': 'true',
+            'UpdateTime': datetime.datetime.now().strftime("%Y-%m-%d" "%H:%M:%S")
+        }
+        print res_data
+        return res_data
